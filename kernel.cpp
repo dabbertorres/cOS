b139444233fa7fcf9e197eec4b86de9313503a2f
Alec Iverson <adiverso93@gmail.com>
2015-03-31 12:07:40 -0700

    Renamed some stuff and added better OS detection

--- kernel.cpp
+++ kernel.cpp
@@ -2,14 +2,12 @@
 
 #define NO_NAME_MANGLE extern "C"
 
-/* Check if the compiler thinks if we are targeting the wrong operating system. */
-#if defined(__linux__)
+#if defined(__linux__) || defined(_WIN32) || defined(__APPLE__) && defined(__MACH__)
 #error "You are not using a cross-compiler, you will most certainly run into trouble"
 #endif
 
-/* This tutorial will only work for the 32-bit ix86 targets. */
 #if !defined(__i386__)
-#error "This tutorial needs to be compiled with a ix86-elf compiler"
+#error "This OS needs to be compiled with a ix86-elf compiler"
 #endif
 
 #include "lib/String.hpp"
