66469caf786318ecfadb35dc9b78a0fecfde85cd
Alec Iverson <adiverso93@gmail.com>
2015-04-01 03:32:45 -0700

    Added long int unsigned and signed usings

--- lib/Types.hpp
+++ lib/Types.hpp
@@ -7,11 +7,13 @@ namespace cos
 	using uint8 = unsigned char;
 	using uint16 = unsigned short int;
 	using uint32 = unsigned int;
+	using uint64 = unsigned long int;
 
 	// signed types
 	using int8 = char;
 	using int16 = short int;
 	using int32 = int;
+	using int64 = long int;
 }
 
 #endif // COS_TYPES_HPP
